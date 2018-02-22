96c2644fdf5f80c9fd0b4daf80e9ba6390bc5e1d
erl67 <erl67@pitt.edu>
2018-02-21 20:31:36 -0500

    error handling / templates

--- cater.py/webtest.py
+++ cater.py/webtest.py
@@ -5,9 +5,10 @@ Created on Mon Feb 19 16:31:53 2018
 @author: E
 """
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, flash, render_template, abort, request, Response
 from flask_sqlalchemy import SQLAlchemy
+
+from webpage import header, footer, setTitle
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -23,8 +24,9 @@ def helloT(name=None):
     return render_template('hello.html', name=name)
 
 @app.route('/')
+@app.route('/base/')
 def index():
-    return header() + setTitle('Flask Test') + '<h1 onclick=\"reColor(\'page\', \'page\');\">Index Page</h1><p></p>' + footer()
+    return Response(render_template('base.html'), status=203, mimetype='text/html')
 
 @app.route('/test/')
 def test():
@@ -34,49 +36,25 @@ def test():
 def about():
     return header() + setTitle('About Page') + '<h4 onclick=\"reColor(\'page\', \'page\');\">About Page</h4>' + footer()
 
-def header():
-    header = """
-    <!DOCTYPE html>
-        <html lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
-        <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="robots" content="noindex">
-        <meta name="googlebot" content="noindex">
-        <meta name="author" content="Eric Laslo">
-        <meta name="description" content="IS1061 ERL67">
-        <link rel="stylesheet" type="text/css" href="./static/style.css">
-        <link rel="icon" type="image/x-icon" href="http://ericlaslo.com/assets/icons/faviconf0.ico">
-        <script src="http://ericlaslo.com/assets/code/footerbar.js" type="text/javascript"></script>
-        <script src="http://ericlaslo.com/assets/code/color.js" type="text/javascript"></script>
-        <title>Flask</title>
-        </head>
-        <body id="page">
-        """
-    return header
-
-def footer():
-    footer = """
-        <script type='text/javascript'>
-            colorize(['page']);
-            colorizeText(['page'], false);
-            footerBar();
-        </script>
-        </body>
-        </html>
-        """
-    return footer
-
-def setTitle(pageTitle):
-    title = "<script>document.title = \'" 
-    title += pageTitle
-    title += "\';</script>"
-    return title
-
 @app.errorhandler(403)
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return Response(render_template('404.html', errno=error), status=404, mimetype='text/html')
+
+@app.route('/404/')
+def error404():
+    abort(404)
+    
+@app.route('/404t/')
+def error404t():
+    return render_template('404.html')
+
+@app.route('/418/')
+def err418(error=None):
+    return Response(render_template('418.html', errno=error), status=418, mimetype='text/html')
 
 if __name__ == "__main__":
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, host='0.0.0.0')
     app.run()
